3e05f7c8936d284a2ff77e923f5f7d662ffa9837
Alex <Alex@MacBook-Pro-di-m.local>
2021-02-09 17:05:08 +0100

    add enter event listener for searchBar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -9,22 +9,33 @@ import './SearchBar.css';
         }
         this.search = this.search.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
     search() {
         this.props.onSearch(this.state.term)
+        console.log('click')
     }
 
     handleSearch(e) {
         this.setState({ term: e.target.value })
     }
-    
+    handleKeyDown(e) {
+        if (e.key === 'Enter'){
+        this.props.onSearch(this.state.term)
+        }
+        console.log('enter')
+    }
     
     render() {
         return (
             <div className="SearchBar">
-                <input onChange={this.handleSearch} placeholder="Enter A Song, Album, or Artist" />
-                <button onClick={this.search}className="SearchButton">SEARCH</button>
+                <input onChange={this.handleSearch} 
+                       placeholder="Enter A Song, Album, or Artist" 
+                       onKeyDown={this.handleKeyDown}/>
+
+                <button onClick={this.search}
+                        className="SearchButton">SEARCH</button>
             </div>
         )
     }
