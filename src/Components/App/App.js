748355a4480598fd81ef420b8476d7ffcf6c3abe
Alex <Alex@MacBook-Pro-di-m.local>
2021-02-09 10:33:05 +0100

    link save playlist to spotify api (not working)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,18 +10,9 @@ import Spotify from '../../util/Spotify'
  class App extends React.Component {
   constructor(props){
     super(props)
-    this.state = { searchResults: [
-                    {name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
-                    {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, 
-                    {name: 'name3', artist: 'artist3', album: 'album3', id: 3},
-                    {name: 'name4', artist: 'artist4', album: 'album4', id: 4}
-                    ],
-                   playlistName: 'defaultPlaylist',
-                   playlistTracks: [
-                    {name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 100}, 
-                    {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 101}, 
-                    {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 102 }, 
-                    ]
+    this.state = { searchResults: [],
+                   playlistName: 'New Playlist',
+                   playlistTracks: []
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -51,7 +42,13 @@ import Spotify from '../../util/Spotify'
   }
 
   savePlaylist() {
-    const trackUri = this.playlistTracks.map(track => track.uri)
+    const trackUris = this.playlistTracks.map(track => track.uri)
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then (() => {
+      this.setState ({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
   }
 
   search(term) {
